61f951cf72265bd68eb511314a0b27e458cac4ee
dn <69494267+dl-mp@users.noreply.github.com>
2020-09-18 00:44:23 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//<10 = failed attendance
+//
+void gpa(int a)
+void atd(int b)
+
+int main()
+{
+    int a, b;
+    printf("Enter GPA: \n");
+    scanf("%d", &a);
+    printf(gpa(int a));
+
+    printf("Enter attendance: \n");
+    scanf("%d", &b)
+    printf(atd(int b));
+
+
+    return 0;
+}
+void gpa(int a)
+{
+    if(a<75)
+        printf("Failed");
+    if else(a>=75)
+        printf("Passed");
+
+   return 0;
+}
+void atd(int b)
+{
+    if(b<10)
+        printf("Failed");
+    if else (b>=20);
+        printf("Passed");
+
+        return 0;
+}
